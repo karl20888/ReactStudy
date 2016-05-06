42f43504ea944d9f0828728d4019b625643b2f64
Karl Liris <karl20888@sohu.com>
2016-05-06 17:59:43 +0800

    56

--- build/.module-cache/1cc2c83d9b574dbda78fb4bff0bed2023dc40e19.js
+++ build/.module-cache/1cc2c83d9b574dbda78fb4bff0bed2023dc40e19.js
@@ -1,5 +1,169 @@
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello, world!'
-), document.getElementById('example'));
+/* HelloWorld
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <p>
+        Hello, <input type="text" placeholder="Your name here" />!
+        It is {this.props.date.toTimeString()}
+      </p>
+    );
+  }
+});
+
+setInterval(function() {
+  ReactDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('example')
+  );
+}, 500);
+*/
+
+/* 渲染HTML标签
+var myDivElement = <div className="foo" />;
+ReactDOM.render(myDivElement, document.getElementById('example'));
+*/
+
+/* 渲染React组件
+var MyCompontent = React.createClass({
+  render: function() {
+    return (
+      <p>MyCompontent</p>
+    )
+  }
+});
+var myElement = <MyCompontent someProperty={true} />;
+ReactDOM.render(myElement, document.getElementById('example'));
+*/
+
+/* 动态交互式用户界面
+var LikeButton = React.createClass({
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(event) {
+    this.setState({liked: !this.state.liked});
+  },
+  render: function() {
+    var text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      <p onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </p>
+    );
+  }
+});
+
+ReactDOM.render(
+  <LikeButton />,
+  document.getElementById('example')
+);
+*/
+
+/* 复合组件
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <PagePic pagename={this.props.pagename} />
+        <PageLink pagename={this.props.pagename} />
+      </div>
+    );
+  }
+});
+
+var PagePic = React.createClass({
+  render: function() {
+    return (
+      <img src={'https://graph.facebook.com/' + this.props.pagename + '/picture'} />
+    );
+  }
+});
+
+var PageLink = React.createClass({
+  render: function() {
+    return (
+      <a href={'https://www.facebook.com/' + this.props.pagename}>
+        {this.props.pagename}
+      </a>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Avatar pagename="Engineering" />,
+  document.getElementById('example')
+);
+*/
+
+/* 验证
+var MyComponent = React.createClass({
+  propTypes: {
+    children: React.PropTypes.number
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.props.children} // 这里必须是一个元素否则就会警告
+      </div>
+    );
+  }
+
+});
+ReactDOM.render(
+  <MyComponent children={'abc'} />,
+  document.getElementById('example')
+);
+*/
+
+/* 传递Props
+var CheckLink = React.createClass({
+  render: function() {
+    return <a {...this.props}>{'√ '}{this.props.children}</a>;
+  }
+});
+
+ReactDOM.render(
+  <CheckLink href="/checked.html">
+    Click here!
+  </CheckLink>,
+  document.getElementById('example')
+);
+*/
+
+var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function() {
+    this.intervals.forEach(clearInterval);
+  }
+};
+
+var TickTock = React.createClass({
+  mixins: [SetIntervalMixin],
+  getInitialState: function() {
+    return {seconds: 0};
+  },
+  componentDidMount: function() {
+    this.setInterval(this.tick, 1000);
+  },
+  tick: function() {
+    this.setState({seconds: this.state.seconds + 1});
+  },
+  render: function() {
+    return (
+      <p>
+        React has been running for {this.state.seconds} seconds.
+      </p>
+    );
+  }
+});
+
+ReactDOM.render(
+  <TickTock />,
+  document.getElementById('example')
+);
